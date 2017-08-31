57f450895cb73f43b26a2b5542e52fe0fc469676
Daniel Dickison <daniel@whoartthou>
2017-08-31 02:48:53 -0400

    Initial Commit

---
+++ Xcode9SwiftProtocolTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode9SwiftProtocolTest
+//
+//  Created by Daniel Dickison on 8/31/17.
+//  Copyright © 2017 Daniel Dickison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
